7fa1debb58cae1d2ab33abec89ac588666854812
Isuru Warnakulasooriya <isuru.warn@gmail.com>
2014-09-05 15:55:18 -0700

    Added the Delete Suggestion feature, and some improvements

--- WebContent/res/js/editor.js
+++ WebContent/res/js/editor.js
@@ -187,11 +187,9 @@ function updateText( elTextArea, newCharCode ) {
 function replaceChars( elTextArea ) {
 	
 	var text = elTextArea.value;
-	
-	// get initial cursor position
-	var cursorPos = elTextArea.selectionStart;
+	var selectionStart = elTextArea.selectionStart;
 	var selectionEnd = elTextArea.selectionEnd;
-	//console.log("replaceChars - cursorPos=" + cursorPos + ", selectionEnd=" + selectionEnd );
+	//console.log("replaceChars - selectionStart=" + selectionStart + ", selectionEnd=" + selectionEnd );
 	
 	text = text.replace( /[A-Za-z]/g, "" );
 	text = text.replace( "`", "" );
@@ -223,16 +221,14 @@ function replaceChars( elTextArea ) {
 	
 	// update textarea and cursor position
 	elTextArea.value = text;
-	elTextArea.selectionStart = cursorPos;
+	elTextArea.selectionStart = selectionStart;
 	elTextArea.selectionEnd = selectionEnd;
 	
 	// save text history in localstorage
 	if( typeof(Storage) !== "undefined" ) {
 		localStorage.mainText = text;
-		//console.log("replaceChars - localStorage.mainText=" + localStorage.mainText );
 	}
 	
-	//console.log("--------------------------------------");
 }
 
 
@@ -370,6 +366,8 @@ function updateAllWordsList( elTextArea ) {
 	for( var i=0; i < currentWordList.length-1; i++ ) {
 		
 		var word = currentWordList[i];
+		//console.log("updateAllWordsList - word=" + word );
+		//console.log("updateAllWordsList - allWordsList.indexOf(word)=" + allWordsList.indexOf(word) );
 		
 		if( word.length >= minimumLength && allWordsList.indexOf(word) === -1 ) {
 			allWordsList.push(word);
@@ -412,13 +410,8 @@ function updateSuggestions( text, selectionEnd  ) {
 		var currentWordLength = 0;
 		
 		//get current word being typed
-		var textSubstring = text.substring( 0, selectionEnd + 1 );
-		textSubstring = textSubstring.replace(/(\r\n|\n|\r)/g, ' ');
-		textSubstring = textSubstring.replace(/\./g, ' ');
-		textSubstring = textSubstring.replace(/\(/g, ' ');
-		textSubstring = textSubstring.replace(/\)/g, ' ');
-		textSubstring = textSubstring.replace(/[0-9]/g, ' ');
-		textSubstring = textSubstring.replace(/,/g, ' ');
+		var textSubstring = text.substring( 0, selectionEnd + 1);
+		textSubstring = replaceSpecialCharsWithSpaces( textSubstring );
 		//console.log("updateSuggestions - textSubstring='" + textSubstring + "'" );
 		
 		for( var i = textSubstring.length-1; i >= 0; i-- ) {
@@ -465,17 +458,24 @@ function updateSuggestions( text, selectionEnd  ) {
 				
 				if( wordSubstring.indexOf( currentWord ) !== -1 && suggestionsList.indexOf(word) === -1 && word !== currentWord && word.length >= minimumLength ) {
 					suggestionsList.push(word);
-					suggestedWordsRows += "<tr><td class=\"sinhalaButton\" onclick=\"appendSuggestion('" + word.replace(currentWord, '') + "')\">" + word + "</td></tr>";
+					suggestedWordsRows += 
+						"<tr>" +
+							"<td>&nbsp;</td>" + 
+							"<td class=\"sinhalaButton\" onclick=\"appendSuggestion('" + word.replace(currentWord, '') + "')\">" + word + "</td>" +
+							"<td><a class=\"delSuggestion\" onclick=\"removeFromAllWordsList('" + word + "')\" title=\"Remove word from suggestions\">&nbsp;X&nbsp;</a></td>" +
+						"</tr>";
 					suggestions++;
 				}
 			}
 			
-			var suggestionsTbl =
-				"<table class=\"suggestionsTable\" title=\"Provides suggestions based on previous words you have typed..\">" +
-					"<caption>Suggestions</caption>" +
-					suggestedWordsRows + 
-				"</table>";
-		
+			var suggestionsTbl = "";
+			if( suggestions > 0 ) {
+				suggestionsTbl =
+					"<table class=\"suggestionsTable\" title=\"Provides suggestions based on previous words you have typed..\">" +
+						"<caption>Suggestions</caption>" +
+						suggestedWordsRows + 
+					"</table>";
+			}
 			document.getElementById("suggestionsHolder").innerHTML = suggestionsTbl;
 		
 		}
@@ -486,14 +486,39 @@ function updateSuggestions( text, selectionEnd  ) {
 
 
 function getCurrentWordsList( text ) {
-	text = text.replace(/(\r\n|\n|\r)/g, ' ');
-	text = text.replace(/\./g, ' ');
-	text = text.replace(/\(/g, ' ');
-	text = text.replace(/\)/g, ' ');
-	text = text.replace(/[0-9]/g, ' ');
-	text = text.replace(/,/g, ' ');
+	text = replaceSpecialCharsWithSpaces( text );
 	return text.split(' ');
 }
+
+
+
+function replaceSpecialCharsWithSpaces( text ) {
+	text = text.replace(/(\r\n|\n|\r)/g, ' ');
+	text = text.replace(/[0-9]/g, ' ');
+	text = text.replace(/\./g, ' ');
+	text = text.replace(/,/g, ' ');
+	text = text.replace(/\(/g, ' ');
+	text = text.replace(/\)/g, ' ');
+	text = text.replace(/:/g, ' ');
+	text = text.replace(/;/g, ' ');
+	text = text.replace(/[\[\]']+/g, ' ');
+	text = text.replace(/{/g, ' ');
+	text = text.replace(/}/g, ' ');
+	text = text.replace(/!/g, ' ');
+	text = text.replace(/#/g, ' ');
+	text = text.replace(/\$/g, ' ');
+	text = text.replace(/&/g, ' ');
+	text = text.replace(/\*/g, ' ');
+	text = text.replace(/=/g, ' ');
+	text = text.replace(/\+/g, ' ');
+	text = text.replace(/-/g, ' ');
+	text = text.replace(/"/g, ' ');
+	text = text.replace(/'/g, ' ');		
+	text = text.replace(/\?/g, ' ');
+	text = text.replace(/\//g, ' ');
+	return text;
+}
+
 
 
 
@@ -519,6 +544,8 @@ function appendSuggestion( word ) {
 	elTextArea.selectionStart = cursorPos + cursorDisplacement;
 	elTextArea.selectionEnd = selectionEnd + cursorDisplacement;
 	elTextArea.focus();
+	
+	document.getElementById("suggestionsHolder").innerHTML = "";
 }
 
 
@@ -527,6 +554,31 @@ function clearMetaData() {
 	localStorage.allWordsList = "";
 	alert("Browser storage cleared!");
 }
+
+
+
+function removeFromAllWordsList( word ) {
+	
+	//console.log("removeFromAllWordsList - word=" + word );
+	
+	if( typeof(Storage) !== "undefined" ) {
+		
+		if( localStorage.allWordsList ) {
+			
+			var replaceString = ',' + word + ',';
+			localStorage.allWordsList = localStorage.allWordsList.replace( replaceString, ',');
+			alert('The word "' + word + '" was removed from browser storage');
+			
+		} else {
+			alert('No words have been saved in browser storage yet..');
+		}
+		
+	} else {
+		alert('Browser does not support local storage');
+	}
+	
+}
+
 
 
 
