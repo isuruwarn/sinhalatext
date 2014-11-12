6fb53015c799a009cc1ee9d42ad1b87ccb9d5b08
Isuru Warnakulasooriya <isuru.warn@gmail.com>
2014-11-11 18:43:54 -0800

    initial commits for mobile and tablet versions

--- WebContent/res/js/editor_mob.js
+++ WebContent/res/js/editor_mob.js
@@ -92,15 +92,13 @@ $("#mainTextArea").bind("keypress", function(event) {
 	if( sinhalaMode ) {
 		
 		var keyInput = event.which;
-		//console.log("handleKeyInput - keyInput=" + keyInput );
+		console.log("handleKeyInput - keyInput=" + keyInput );
 		
 		var metaKey = event.metaKey; // for mac command key
 		var controlKey = event.ctrlKey;
 		
 		if( keyInput == 32 || keyInput == 13 ) {
-			
-			updateAllWordsList( this );
-			$("#suggestionsHolder").html("");
+			return;
 			
 		} else if( keyInput < 60 || keyInput == 61 || keyInput == 63 || controlKey || metaKey ) {
 			return;
@@ -122,7 +120,7 @@ $("#mainTextArea").bind("keypress", function(event) {
 function updateText( elTextArea, keyInput ) {
 	
 	var newChar = charMap[ keyInput ].UNICODE;
-	//console.log("updateText - newChar=" + newChar );
+	console.log("updateText - newChar=" + newChar );
 	
 	if( newChar ) {
 		
@@ -153,7 +151,6 @@ function updateText( elTextArea, keyInput ) {
 		}
 		
 		fixUnicodeIssues( elTextArea );
-		updateSuggestions( elTextArea.value, elTextArea.selectionEnd );
 		updateTextInLocalStorage( elTextArea.value );
 	}
 	
@@ -220,387 +217,36 @@ function clearAll() {
 
 
 
-function clearMetaData() {
-	localStorage.allWordsList = "";
-	alert("Browser storage cleared!");
-}
-
-
-
-function printPreview() {
-	var html = preparePrintPage();
-	var popup = window.open("", "Print Preview", "width=720, height=900, scrollbars=yes, resizable=yes, menubar=no, status=no, titlebar=no, toolbar=no" );
-	popup.document.open();
-	popup.document.write(html);
-}
-
-
-
-function print() {
-	var html = preparePrintPage();
-	var popup = window.open("", "Print", "width=720, height=900, scrollbars=yes, resizable=yes, menubar=no, status=no, titlebar=no, toolbar=no" );
-	popup.document.open();
-	popup.document.write(html);
-	popup.print();
-}
-
-
-
-function preparePrintPage() {
-	
-	var elTextArea = document.getElementById("mainTextArea");
-	var text = elTextArea.value;
-	var fontSize = elTextArea.style.fontSize;
-	
-	text = text.replace(/\n/g, "<br>");
-	var style = 
-		"<style>" +
-		"	.mainText {" +
-		"		font-size: " + fontSize + "; " +
-		"	}" +
-		"</style>";
-		var head = "<head>" + style + "</head>";
-	var body = "<body class='mainText'>" + text + "</body>";
-	var html = "<html>" + head + body + "</html>";
-	
-	return html;
-}
-
-
-
-
-function changeFontSize() {
-	var selFontSize = document.getElementById("selFontSize");
-	var newFontSize = selFontSize.value;
-	var elTextArea = document.getElementById("mainTextArea");
-	elTextArea.style.fontSize = newFontSize + 'px';
-}
-
-
-
-
-function toggleRightTray( toggleBtnDiv, holderDivName ) {
-	
-	var holderDiv = document.getElementById( holderDivName );
-	
-	if( toggleBtnDiv.innerHTML === "&gt;&gt;" ) {
-		holderDiv.className = holderDivName + "Show divTransitions rightTray";
-		toggleBtnDiv.innerHTML = "&lt;&lt;";
-		
-	} else {
-		holderDiv.className = holderDivName + "Hidden divTransitions rightTray";
-		toggleBtnDiv.innerHTML = "&gt;&gt;";
-	}
-}
-
-
-
-
-
-function updateAllWordsList( elTextArea ) {
-	
-	var newWords = 0;
-	var minimumLength = 2;
-	var text = elTextArea.value;
-	var currentWordList = getCurrentWordsList(text);
-	var allWordsList = window.localStorage.allWordsList;
-	
-	// initialize all words list
-	if( typeof(Storage) !== "undefined" ) {
-		
-		if( localStorage.allWordsList ) {
-			allWordsList = localStorage.allWordsList.split(',');
-			
-		} else {
-			localStorage.allWordsList = "";
-			allWordsList = [];
-		}
-	}
-	
-	// check for any new words (except current word being typed)
-	for( var i=0; i < currentWordList.length-1; i++ ) {
-		
-		var word = currentWordList[i];
-		
-		if( word.length >= minimumLength && allWordsList.indexOf(word) === -1 ) {
-			allWordsList.push(word);
-			newWords++;
-		}
-	}
-	
-	// update all words list in localStorage
-	if( newWords > 0 && typeof(Storage) !== "undefined" ) {
-		
-		// sort words by length, ascending
-		allWordsList.sort( function( a, b) {
-			if( a.length > b.length )
-				return 1;
-			if (a.length < b.length )
-				return -1;
-			return 0;
-		});
-		
-		localStorage.allWordsList = allWordsList;
-	}
-	
-	updateTextInLocalStorage( elTextArea.value );
-	
-}
-
-
-
-function updateSuggestions( text, selectionEnd  ) {
-	
-	var allWordsList = [];
-	var suggestionsList = [];
-	var maxSuggestions = 10;
-	var minimumLength = 3;
-	var suggestions = 0;
-	var currentWordList = getCurrentWordsList(text);
-	
-	if( currentWordList.length > 0 ) {
-		
-		var currentWord = "";
-		var currentWordLength = 0;
-		
-		//get current word being typed
-		var textSubstring = text.substring( 0, selectionEnd + 1);
-		textSubstring = replaceSpecialCharsWithSpaces( textSubstring );
-		
-		for( var i = textSubstring.length-1; i >= 0; i-- ) {
-			
-			var currentChar = textSubstring[i];
-			
-			if( currentChar === ' ' ) {
-				currentWord = textSubstring.substring( i+1, textSubstring.length );
-				currentWordLength = currentWord.length;
-				break;
-				
-			} else if ( i == 0 ) {
-				currentWord = textSubstring.substring( i, textSubstring.length );
-				currentWordLength = currentWord.length;
-				break;
-			}
-		}
-		
-		if( currentWordLength > 0 ) {
-		
-			// get list of known words
-			if( typeof(Storage) !== "undefined" ) {
-				if( localStorage.allWordsList ) { // first look in localstorage
-					allWordsList = localStorage.allWordsList.split(',');
-				} else { // if local storage is empty, use current word list
-					allWordsList = currentWordList;
-				}
-			}
-			
-			var suggestedWordsRows = "";
-			for( var i=0; i < allWordsList.length; i++ ) {
-				
-				if( suggestions >= maxSuggestions ) {
-					break;
-				}
-				
-				var word = allWordsList[i];
-				var wordSubstring = word.substring( 0, currentWordLength );
-				
-				if( wordSubstring.indexOf( currentWord ) !== -1 && suggestionsList.indexOf(word) === -1 && word !== currentWord && word.length >= minimumLength ) {
-					suggestionsList.push(word);
-					suggestedWordsRows += 
-						"<tr>" +
-							"<td>&nbsp;</td>" + 
-							"<td class=\"sinhalaButton\" onclick=\"appendSuggestion('" + word.replace(currentWord, '') + "')\">" + word + "</td>" +
-							"<td><a class=\"delSuggestion\" onclick=\"removeFromAllWordsList('" + word + "')\" title=\"Remove word from suggestions\">&nbsp;X&nbsp;</a></td>" +
-						"</tr>";
-					suggestions++;
-				}
-			}
-			
-			var suggestionsTbl = "";
-			if( suggestions > 0 ) {
-				suggestionsTbl =
-					"<table class=\"suggestionsTable\" title=\"Provides suggestions based on previous words you have typed..\">" +
-						"<caption>Suggestions</caption>" +
-						suggestedWordsRows + 
-					"</table>";
-			}
-			$("#suggestionsHolder").html(suggestionsTbl);
-		
-		}
-
-	}
-}
-
-
-
-function getCurrentWordsList( text ) {
-	text = replaceSpecialCharsWithSpaces( text );
-	return text.split(' ');
-}
-
-
-
-function replaceSpecialCharsWithSpaces( text ) {
-	text = text.replace( /[A-Za-z]/g, "" );
-	text = text.replace( "`", "" );
-	text = text.replace( "~", "" );
-	text = text.replace( "@", "" );
-	text = text.replace( "^", "" );
-	text = text.replace( "_", "" );
-	text = text.replace( "[", "" );
-	text = text.replace( "{", "" );
-	text = text.replace( "]", "" );
-	text = text.replace( "}", "" );
-	text = text.replace( "\\", "" );
-	text = text.replace( "|", "" );
-	text = text.replace( "<", "" );
-	text = text.replace( ">", "" );
-	text = text.replace(/(\r\n|\n|\r)/g, ' ');
-	text = text.replace(/[0-9]/g, ' ');
-	text = text.replace(/\./g, ' ');
-	text = text.replace(/,/g, ' ');
-	text = text.replace(/\(/g, ' ');
-	text = text.replace(/\)/g, ' ');
-	text = text.replace(/:/g, ' ');
-	text = text.replace(/;/g, ' ');
-	text = text.replace(/[\[\]']+/g, ' ');
-	text = text.replace(/{/g, ' ');
-	text = text.replace(/}/g, ' ');
-	text = text.replace(/!/g, ' ');
-	text = text.replace(/#/g, ' ');
-	text = text.replace(/\$/g, ' ');
-	text = text.replace(/&/g, ' ');
-	text = text.replace(/\*/g, ' ');
-	text = text.replace(/=/g, ' ');
-	text = text.replace(/\+/g, ' ');
-	text = text.replace(/-/g, ' ');
-	text = text.replace(/"/g, ' ');
-	text = text.replace(/'/g, ' ');		
-	text = text.replace(/\?/g, ' ');
-	text = text.replace(/\//g, ' ');
-	return text;
-}
-
-
-
-
-function appendSuggestion( word ) {
-	
-	var elTextArea = document.getElementById("mainTextArea");
-	var text = elTextArea.value;
-	var lengthBfrAppend = text.length;
-	var cursorPos = elTextArea.selectionStart;
-	var selectionEnd = elTextArea.selectionEnd;
-	var start = 0;
-	var end = text.length;
-	var head = text.substring( start, cursorPos );
-	var tail = text.substring( cursorPos, end );
-	text = head + word + tail;
-	
-	// get initial cursor position
-	var lengthAfterAppend = text.length;
-	var cursorDisplacement = lengthAfterAppend - lengthBfrAppend;
-	
-	// update text area and cursor position
-	elTextArea.value = text;
-	elTextArea.selectionStart = cursorPos + cursorDisplacement;
-	elTextArea.selectionEnd = selectionEnd + cursorDisplacement;
-	elTextArea.focus();
-	
-	updateTextInLocalStorage( elTextArea.value );
-	$("#suggestionsHolder").html("");
-}
-
-
-
-
-function clearMetaData() {
-	localStorage.allWordsList = "";
-	alert("Browser storage cleared!");
-}
-
-
-
-function removeFromAllWordsList( word ) {
-	
-	//console.log("removeFromAllWordsList - word=" + word );
-	
-	if( typeof(Storage) !== "undefined" ) {
-		
-		if( localStorage.allWordsList ) {
-			
-			var replaceString = ',' + word + ',';
-			localStorage.allWordsList = localStorage.allWordsList.replace( replaceString, ',');
-			alert('The word "' + word + '" was removed from browser storage');
-			
-		} else {
-			alert('No words have been saved in browser storage yet..');
-		}
-		
-	} else {
-		alert('Browser does not support local storage');
-	}
-	
-}
-
-
-
-
 function createKeyMapTable() {
 	
 	var mainKMTable = $("<table></table>").addClass("keyMapTable");
 	var captionMainTable = $("<caption></caption>").text("Key Map");
-	
+	var trMainTable = $("<tr></tr>");
 	var keys = [97, 65, 98, 66, 99, 67, 100, 68, 101, 69, 102, 70, 103, 71, 104, 72, 105, 73, 106, 74, 107, 75, 108, 76, 109, 77, 110, 78, 111, 79, 112, 80, 113, 81, 114, 82, 115, 83, 116, 84, 117, 85, 118, 86, 119, 87, 120, 88, 121, 89, 122, 90, 92, 124, 96, 126, 64, 94, 95, 91, 123, 93, 125, 60, 62 ];
+	var keyCount = 0;
 	
-	for( var i=0; i<22; i++ ) {
+	for( var i=0; i<65; i++ ) {
 		
-		var trMainTable = $("<tr></tr>");
-		
-		var key = keys[i];
-		var symbol = charMap[key];
-		var td1 = $("<td>" + symbol.CHARCODE + "</td>");
-		var td2 = $("<td>-</td>");
-	    var td3 = $("<td>" + symbol.HTMLCODE + "</td>").addClass("sinhalaButton").attr( { title: symbol.NAME, onclick: "appendChar(" + key + ")"  } );
-	    var td4 = $("<td>&nbsp;&nbsp;</td>").attr( { width: "20px" } );
-	    
-    	var td5 = $("<td></td>");
-    	var td6 = $("<td></td>");
-    	var td7 = $("<td></td>");
-    	var td8 = $("<td></td>");
-	    key = keys[i+22];
+    	var td = $("<td></td>");
+	    var key = keys[i];
+	    var symbol = null;
 	    if(key) {
 			symbol = charMap[key];
-		    td5 = $("<td>" + symbol.CHARCODE + "</td>");
-			td6 = $("<td>-</td>");
-		    td7 = $("<td>" + symbol.HTMLCODE + "</td>").addClass("sinhalaButton").attr( { title: symbol.NAME, onclick: "appendChar(" + key + ")"  } );
-		    td8 = $("<td>&nbsp;&nbsp;</td>").attr( { width: "20px" } );
+		    td = $("<td>" + symbol.HTMLCODE + "</td>").addClass("sinhalaButton").attr( { title: symbol.NAME, onclick: "appendChar(" + key + ")"  } );
 	    }
 	    
-    	var td9 = $("<td></td>");
-    	var td10 = $("<td></td>");
-    	var td11 = $("<td></td>");
-	    key = keys[i+45];
-	    if(key) {
-			symbol = charMap[key];
-		    td9 = $("<td>" + symbol.CHARCODE + "</td>");
-			td10 = $("<td>-</td>");
-		    td11 = $("<td>" + symbol.HTMLCODE + "</td>").addClass("sinhalaButton").attr( { title: symbol.NAME, onclick: "appendChar(" + key + ")"  } );
+	    trMainTable.append(td);
+	    keyCount++;
+	    
+	    if( keyCount % 10 == 0 ) {
+	    	mainKMTable.append(trMainTable);
+	    	trMainTable = $("<tr></tr>");
+	    	keyCount = 0;
 	    }
 	    
-	    trMainTable.append(td1);
-	    trMainTable.append(td2);
-	    trMainTable.append(td3);
-	    trMainTable.append(td4);
-	    trMainTable.append(td5);
-	    trMainTable.append(td6);
-	    trMainTable.append(td7);
-	    trMainTable.append(td8);
-	    trMainTable.append(td9);
-	    trMainTable.append(td10);
-	    trMainTable.append(td11);
-	    mainKMTable.append(trMainTable);
 	}
+	
+	mainKMTable.append(trMainTable);
 	
 	mainKMTable.append(captionMainTable);
 	$('#keyMapDiv').append(mainKMTable);
@@ -641,12 +287,15 @@ function restorePreviousText() {
 
 
 
+
 function updateTextInLocalStorage( text ) {
 	// save text history in localstorage
 	if( typeof(Storage) !== "undefined" ) {
 		localStorage.mainText = text;
 	}
 }
+
+
 
 
 
